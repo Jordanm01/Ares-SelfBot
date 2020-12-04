c0f6942b96e75efd665c1de0a90a0c2fbe2ae588
JordanM01 <M1804176@middlesbro.ac.uk>
2020-12-04 21:19:51 +0000

    Added extra config options for nitro sniping and message logging

--- config.example.py
+++ config.example.py
@@ -1,5 +1,12 @@
 TOKEN = "TOKEN"
-PREFIX = "PREFIX"
-PASSWORD = "PASSWORD"  # For setavatar
-WATCHED_SERVERS = [123, 456, 789]  # Server ID's to look for deleted messages in!
+REDEEM_TOKEN = "TOKEN"  # The token of the account you wish to snipe nitro onto
+PASSWORD = "PASSWORD"  # For set avatar / avatar stealer
+
+PREFIX = "&"
 EMBED_COLOUR = "000000"
+
+NITRO_SNIPER = True
+NOTIFICATION_WEBHOOK = "WEBHOOK URL"  # The webhook URL if you wish to receive notifications (Nitro Sniper)
+
+LOGGING = True
+WATCHED_SERVERS = [123, 456, 789]  # Server ID's to look for deleted messages in!
